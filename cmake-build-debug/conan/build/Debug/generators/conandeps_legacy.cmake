38bf82545022b4e793f31ecc301732f25562eb0c
Piotr Lasoń <123111159+lasonpeter@users.noreply.github.com>
2025-04-02 22:25:46 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/conan/build/Debug/generators/conandeps_legacy.cmake
+++
@@ -1,7 +0,0 @@
-message(STATUS "Conan: Using CMakeDeps conandeps_legacy.cmake aggregator via include()")
-message(STATUS "Conan: It is recommended to use explicit find_package() per dependency instead")
-
-find_package(FastNoise2)
-find_package(raylib)
-
-set(CONANDEPS_LEGACY  FastNoise2::FastNoise  raylib )
