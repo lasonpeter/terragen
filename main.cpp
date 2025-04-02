5a2482ac6fa50111da317968805be8d24795fd24
Piotr Lasoń <123111159+lasonpeter@users.noreply.github.com>
2025-04-02 22:23:39 +0200

    feat: gitignore update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 ﻿#include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raymath.h"
+#include "raygui.h"
 #include "FastNoise/FastNoise.h"
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -9,8 +12,22 @@ int main(void)
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "terragen");
+    Camera3D camera = {0};
+    camera.position = Vector3{1.0f, 1.0f, 1.0f}; // Camera position
+    camera.target = Vector3{0.0f, 0.0f, 0.0f}; // Camera looking at point
+    camera.up = Vector3{0.0f, 1.0f, 0.0f}; // Camera up vector (rotation towards target)
+    camera.fovy = 70.0f; // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;
+    SetTargetFPS(60);
+    //movement and such
+    float speed = 0.05f;
+    float movement_speed = 0.2f;
+    Vector3 camera_position = {0, 0, 0};
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetExitKey(KEY_NULL);
+    HideCursor();
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -18,6 +35,50 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+
+        ///
+        ///TEMPORARY CAMERA MOVEMENT
+        ///
+        ///
+        Vector3 camera_change = {0, 0, 0};
+        if (IsKeyDown(KEY_W)) {
+            camera_change.z = 1;
+        }
+        if (IsKeyDown(KEY_S)) {
+            camera_change.z = 1;
+        }
+        if (IsKeyDown(KEY_A)) {
+            camera_change.x = 1;
+        }
+        if (IsKeyDown(KEY_D)) {
+            camera_change.x = 1;
+        }
+        if (IsKeyDown(KEY_SPACE)) {
+            camera_change.y = 1;
+        }
+
+        Vector3 camera_move = {0, 0, 0};
+
+            camera_move = {
+                GetMouseDelta().x * speed, // Rotation: yaw
+                GetMouseDelta().y * speed, // Rotation: pitch
+                0.0f // Rotation: roll
+            };
+            SetMousePosition(GetRenderWidth() / 2, GetRenderHeight() / 2);
+        ClearBackground(WHITE);
+        Vector3Scale(camera_change, speed);
+        camera_position = Vector3Add(camera_position, camera_change);
+        UpdateCameraPro(&camera,
+                        Vector3{
+                            (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) * movement_speed - // Move forward-backward
+                            (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) * movement_speed,
+                            (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) * movement_speed - // Move right-left
+                            (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) * movement_speed,
+                            -(IsKeyDown(KEY_C)) * movement_speed + (IsKeyDown(KEY_SPACE)) * movement_speed
+                            // Move up-down
+                        }, camera_move,
+                        GetMouseWheelMove() * 2.0f);
+
         // Update
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
@@ -26,10 +87,13 @@ int main(void)
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
+        ClearBackground(RAYWHITE);
+        BeginMode3D(camera);
+        DrawCube( Vector3{0,0,0}, .5f, .5f, .5f,BLACK);
+        EndMode3D();
 
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawCubeWires( Vector3{0,0,0}, .5f, .5f, .5f,BLACK);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
